ce6485466b5ac6f332950fbb8f7def3a5b024ce6
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-16 13:45:20 +0300

    :rewind: Reverted mistakenly made changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,70 +1,40 @@
 #!/usr/bin/python3
-"""
-This module defines the FileStorage class.
-It manages storage of hbnb models in JSON format and handles their restoration.
-
-It's not meant to be used directly in this project, but rather through
-instances of the Storage class defined in __init__.py.
-
-Dependencies:
-    * json module
-    * BaseModel class defined in models/base_model.py
-    * User class defined in models/user.py
-    * Place class defined in models/place.py
-    * State class defined in models/state.py
-    * City class defined in models/city.py
-    * Amenity class defined in models/amenity.py
-    * Review class defined in models/review.py
-"""
-import json  # For serialization/deserialization
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
 
 
 class FileStorage:
-    """
-    Manages storage and retrieval of hbnb models in JSON format.
-
-    Attributes:
-        __file_path (str): Path to the JSON file used for storage.
-        __objects (dict): Dictionary of dictionaries containing all objects.
-
-    Methods:
-        all(self): Returns the dictionary __objects.
-        new(self, obj): Sets in __objects the obj with key <obj class name>.id
-        save(self): Serializes __objects to the JSON file (path: __file_path)
-    """
+    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """
-        Return all currently stored objects in a dictionary.
-        """
+    def all(self, cls=None):
+        """Returns a dictionary or a filtered dictionary of models currently in storage"""
+        if cls:
+            return {key: obj for key, obj in FileStorage.__objects.items() if isinstance(obj, cls)}
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Add a new object to the current database session.
-        Format: <obj class name>.id : <object>
-        """
+        """Adds new object to storage dictionary"""
         key = obj.to_dict()['__class__'] + '.' + obj.id
         self.all()[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {key: obj.to_dict() for key, obj in
-                    FileStorage.__objects.items()}
+            temp = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
