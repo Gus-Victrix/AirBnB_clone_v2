3de30499352583b05a31ee1e506d469c4ecf0fce
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-17 12:42:29 +0300

    :sparkles: feature: Added database storage functionality to Amenity class.

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python3
 """
-Amenity class that inherits from BaseModel
+Module defines the Amenity class which inherits from BaseModel and Base.
+
+Classes:
+    Amenity: Defines amenities that may be available at a place.
+
+Dependencies:
+    BaseModel: Parent class.
+    Base: Instance of declarative_base.
+    storage_type: The type of storage.
+    Column: An instance of SQLAlchemy Column.
+    String: An instance of SQLAlchemy String.
+    relationship: An instance of SQLAlchemy relationship.
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, storage_type
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
     """
     Defines amenities that may be available at a place.
+    Child table of Place.
+
+    Attributes:
+        name (str): The name of the amenity.
+        __tablename__ (str): The name of the table in the database.
+        place_amenities (Place): The relationship between the amenity and
+            the place.
     """
-    name = ""
+    if storage_type == "db":  # In the case that the storage type is a database
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
+    else:  # In the case that the storage type is a JSON file
+        name = ""
