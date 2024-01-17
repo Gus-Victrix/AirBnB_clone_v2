0674f579565890588be5f40a73b6e6ec159d01e0
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-17 04:00:53 +0300

    :sparkles: feature: Integrated db_storage module into console.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':  # if storage type is database
+    from models.engine.db_storage import DBStorage as Storage
+else:
+    from models.engine.file_storage import FileStorage as Storage
 
-storage = FileStorage()
+storage = Storage()  # instantiates storage object
 storage.reload()
