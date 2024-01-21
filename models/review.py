9cee4d37154dfa2bb1c1491393fadcaecf3d06ea
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-21 04:47:12 +0300

    :bug: bugfix: A few more classes liberated from an excess of ids

--- models/review.py
+++ models/review.py
@@ -11,7 +11,6 @@ class Review(BaseModel, Base):
     if storage_type == "db":
         __tablename__ = "reviews"
         text = Column(String(1024), nullable=False)
-        id = Column(String(60), primary_key=True)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     else:
