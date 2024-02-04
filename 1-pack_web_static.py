74edce6b451f85bd494f4f596fed89ea21b9c760
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-05 00:54:24 +0300

    feature: Added fabric tgz archiving capability for tgz generation

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Fabric script to generate a .tgz archive from the contents of the web_static.
+"""
+
+from fabric.api import local  # run commands locally
+from datetime import datetime  # to get the current date and time
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static.
+    format: web_static_<year><month><day><hour><minute><second>.tgz
+
+    Returns:
+        str: the archive path if the archive has been successfully generated.
+        None: if the archive has not been generated.
+    """
+    local("mkdir -p versions")  # create the folder versions if not exists
+    date = datetime.now().strftime("%Y%m%d%H%M%S")  # get the current date and time
+    file = "versions/web_static_{}.tgz".format(date)  # create the file name
+    result = local("tar -cvzf {} web_static".format(file))  # create the .tgz file
+    if result.failed:  # if the creation of the archive has failed
+        return None
+    return file  # return the archive path
