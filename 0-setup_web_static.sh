9252870f57b5058711c9a3d74b1a59413e288c4f
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-05 01:36:23 +0300

    refactor: Refactored the setup script for the server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
-# Install nginx and create folders
-# Update apt repositories
-apt-get update
-# Install nginx
+#Preconfigure the server to put our static web
+
+apt-get -y update
 apt-get -y install nginx
-# Create folders
-mkdir -p /data/web_static/{releases/test/,shared}
-# Create simple html file
-echo "Holberton School" > /data/web_static/releases/test/index.html
-# Create symbolic link
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Hello world" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Give ownership of the /data/ folder to the ubuntu user AND group
 chown -hR ubuntu:ubuntu /data/
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sed -i "38i \tlocation /hbnb_static/ {\n \t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
-# Restart nginx
+sed -i "38i\\\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 service nginx restart
