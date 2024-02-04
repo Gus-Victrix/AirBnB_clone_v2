be6c82251cde497e1beb045251ecf8634b4d446d
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-05 01:20:54 +0300

    style: Solved pycodestyle for module

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,6 +7,7 @@ Fabric script to generate a .tgz archive from the contents of the web_static.
 from fabric.api import local  # run commands locally
 from datetime import datetime  # to get the current date and time
 
+
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static.
@@ -17,9 +18,9 @@ def do_pack():
         None: if the archive has not been generated.
     """
     local("mkdir -p versions")  # create the folder versions if not exists
-    date = datetime.now().strftime("%Y%m%d%H%M%S")  # get the current date and time
+    date = datetime.now().strftime("%Y%m%d%H%M%S")  # get current datetime
     file = "versions/web_static_{}.tgz".format(date)  # create the file name
-    result = local("tar -cvzf {} web_static".format(file))  # create the .tgz file
+    result = local("tar -cvzf {} web_static".format(file))  # create .tgz file
     if result.failed:  # if the creation of the archive has failed
         return None
     return file  # return the archive path
